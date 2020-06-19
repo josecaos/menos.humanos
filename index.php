318d816dabe19acb656553d10fa86b00c460f57f
josecaos <jazzvoon@gmail.com>
2020-06-18 22:02:00 -0500

    Landing page con logo

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0"/>
+  <meta name="apple-mobile-web-app-capable" content="yes"/>
+  <meta name="apple-mobile-web-app-status-bar-style" content="black"/>
+  <meta name="keywords" content="">
+  <meta name="description" content="">
+  <title>Menos Humanos | Música</title>
+  <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+  <section id="portada">
+
+    <div class="contenido">
+      <div class="logo">
+        <img src="assets/imgs/logo.menos.humanos.png" alt="Menos Humanos">
+      </div>
+
+      <!-- <div class="redes">
+        <ul>
+          <?php
+          //$redes = array('instagram');
+          //$links = array('https://instagram.com');
+          // for ($i=0; $i < count($redes) ; $i++):
+            ?>
+
+            <li>
+              <p>
+                <a href="<?php //echo $links; x?>"><?php //echo $redes[$i]; ?></a>
+              </p>
+            </li>
+
+          <?php //endfor; ?>
+        </ul>
+      </div> -->
+    </div>
+
+  </section>
+</body>
+</html>
